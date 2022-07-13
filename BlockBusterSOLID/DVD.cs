06d18c315b64b4ca04db24f948f721d23bb0c93e
Jeo <jeo961@gmail.com>
2022-07-13 13:44:42 -0400

    first commit

---
+++ BlockBusterSOLID/DVD.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockBusterSOLID
+{
+    internal class DVD : Movie
+    {
+        public DVD(string Title, Genre Category, int Runtime, List<string> Scenes)
+            : base(Title, Category, Runtime, Scenes)
+        {
+
+        }
+
+        public override void Play()
+        {
+            Console.WriteLine($"Select what scene you'd like to watch: 0 to {Scenes.Count - 1}");
+            string input = Console.ReadLine();
+            int index = int.Parse(input);
+
+            string scene = Scenes[index];
+            Console.WriteLine(scene);
+
+        }
+    }
+}
---
+++ BlockBusterSOLID/VHS.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockBusterSOLID
+{
+    internal class VHS : Movie
+    {
+        //We want current time to always start at 0
+        //You may give properties initial values 
+        public int CurrentTime { get; set; } = 0;
+
+        //Base - reference to the parent class 
+        //If the parent has parameters in its constructor, 
+        //The child must call base in it's own constructor to pass values up to the parent 
+        //Outside of Constructors, base functions much like the this keyword, 
+        //but instead of pointing to itself, it point to its immediate parent
+        public VHS(string Title, Genre Category, int Runtime, List<string> Scenes)
+            : base(Title, Category, Runtime, Scenes)
+        {
+
+        }
+
+        public void Rewind()
+        {
+            CurrentTime = 0;
+        }
+
+        //VHS wants it own version of the play method 
+        public override void Play()
+        {
+            if (CurrentTime < Scenes.Count)
+            {
+                string scene = Scenes[CurrentTime];
+                Console.WriteLine(scene);
+                CurrentTime++;
+            }
+            else
+            {
+                Rewind();
+            }
+        }
+    }
+}
